085884a4ff6b79706a9347d23cb35e9beb170141
CheyenneRowe <lanxinlai@126.com>
2022-03-25 12:04:36 +0800

    test pr

--- src/app/modules/main-page/main-page.tsx
+++ src/app/modules/main-page/main-page.tsx
@@ -24,7 +24,7 @@ export default class MainPage extends PureComponent<IMainPageProps, IMainPageSta
     constructor(props) {
         super(props);
         this.state = {
-
+            test:''
         };
     }
 
